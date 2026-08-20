338514b6b3879ecd2c0f9ff9b3915f296ad116e9
Ryanyzh <ryanyong1621@gmail.com>
2026-08-21 03:02:09 +0800

    [Level 0] Rename chatbot to Bobby

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,14 @@
+/**
+ * Entry point for the Bobby chatbot application.
+ */
+public class Bobby {
+    public static void main(String[] args) {
+        String banner = " ____        _     _           \n"
+                + "| __ )  ___ | |__ | |__  _   _ \n"
+                + "|  _ \\ / _ \\| '_ \\| '_ \\| | | |\n"
+                + "| |_) | (_) | |_) | |_) | |_| |\n"
+                + "|____/ \\___/|_.__/|_.__/ \\__, |\n"
+                + "                         |___/ \n";
+        System.out.println(banner);
+    }
+}
