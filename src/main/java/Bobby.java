6bff11185740d8fe5d184a6e48bbc8a7645d9fa8
Ryanyzh <ryanyong1621@gmail.com>
2026-08-21 10:20:29 +0800

    [Level 0] Add initial greeting and exit message

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -2,6 +2,8 @@
  * Entry point for the Bobby chatbot application.
  */
 public class Bobby {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " ____        _     _           \n"
                 + "| __ )  ___ | |__ | |__  _   _ \n"
@@ -9,6 +11,12 @@ public class Bobby {
                 + "| |_) | (_) | |_) | |_) | |_| |\n"
                 + "|____/ \\___/|_.__/|_.__/ \\__, |\n"
                 + "                         |___/ \n";
-        System.out.println(banner);
+        System.out.println(LINE);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Bobby.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
