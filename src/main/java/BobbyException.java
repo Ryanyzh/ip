ff45ac5d0ba86751eb5c16c4f31e72d2e53a9a12
Ryanyzh <ryanyong1621@gmail.com>
2026-08-21 11:36:50 +0800

    [Level 5] Add Bobby-specific exception

---
+++ src/main/java/BobbyException.java
@@ -0,0 +1,13 @@
+/**
+ * Represents an error caused by invalid user input in Bobby.
+ */
+public class BobbyException extends Exception {
+    /**
+     * Creates an exception with a user-facing error message.
+     *
+     * @param message explanation of what went wrong
+     */
+    public BobbyException(String message) {
+        super(message);
+    }
+}
