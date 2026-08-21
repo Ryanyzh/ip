9cf9e798d867a0d18b365810f4c11b424da701e5
Ryanyzh <ryanyong1621@gmail.com>
2026-08-21 10:27:39 +0800

    [Level 1] Echo commands until bye

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Bobby chatbot application.
  */
 public class Bobby {
     private static final String LINE = "____________________________________________________________";
+    private static final String BYE_COMMAND = "bye";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String banner = " ____        _     _           \n"
                 + "| __ )  ___ | |__ | |__  _   _ \n"
                 + "|  _ \\ / _ \\| '_ \\| '_ \\| | | |\n"
@@ -16,7 +20,19 @@ public class Bobby {
         System.out.println("Hello! I'm Bobby.");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(LINE);
+
+            if (command.equals(BYE_COMMAND)) {
+                System.out.println("Goodbye! Bobby signing out...");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(LINE);
+        }
     }
 }
